4c38e63defef41b4c6249b6c9e7cd7f48647de8e
adiazarana <adiaz@pavadiazarana.com>
2023-06-08 14:02:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ title = """
 st.markdown(title, unsafe_allow_html=True)
 st.write("Soy ARIEL®, tu asistente para la redacción inteligente de escritos legales. En este módulo, utilizo inteligencia artificial con tecnología de procesamiento natural del lenguaje para transcribir, organizar y resumir tus archivos de audio a texto. Reconozco la mayoría de los formatos usuales (.mp4, .mp4, .m4a, etc.).\n\n Intentaré ser lo más fiel posible al contenido original. Sin embargo, si no entiendo lo que se dice, usaré el contexto para dar sentido a la transcripción. \n\n Recuerda: estoy en fase de entrenamiento, así que siempre revisa el producto final y contrástalo con el audio que has cargado.\n")
 
-
-
 model = "gpt-4"
 
 uploaded_audio = st.file_uploader("Selecciona un archivo:", type=['m4a', 'mp3', 'webm', 'mp4', 'mpga', 'wav', 'mpeg'], accept_multiple_files=False)
@@ -24,14 +22,16 @@ custom_prompt = None
 
 custom_prompt = st.text_input("Configura el resultado, si así lo deseas:", value = "Haz un resumen basado en la siguiente transcripción")
 
+processing_message = st.empty() # added this line
+
 if st.button("Empezar"):
     if uploaded_audio:
         if api_key:
-            st.markdown("Transcribiendo el audio...")
+            processing_message.text("Transcribiendo el audio...") # changed from st.markdown to processing_message.text
             transcript = transcribe_audio(api_key, uploaded_audio)
             st.markdown(f"###  Trascripción:\n\n<details><summary>Click to view</summary><p><pre><code>{transcript.text}</code></pre></p></details>", unsafe_allow_html=True)
 
-            st.markdown("Procesando la transcripción...")
+            processing_message.text("Procesando la transcripción...") # changed from st.markdown to processing_message.text
             if custom_prompt:
                 summary = summarize_transcript(api_key, transcript, model, custom_prompt)
             else:
@@ -47,6 +47,6 @@ if st.button("Empezar"):
                 file_name="version_procesada.txt",
                 mime="text/plain",
             )
-            processing_message.empty()
+            processing_message.empty() # added this line
         else:
             st.error("Please enter a valid OpenAI API key.")
