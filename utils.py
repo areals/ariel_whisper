c95b7e445a7f6affb1fb0bfb9c5ddbc0ae0b40fa
adiazarana <adiaz@pavadiazarana.com>
2023-07-31 19:55:57 -0500

    Utils +pydub

--- utils.py
+++ utils.py
@@ -1,44 +1,19 @@
 import openai
-import pydub
-from io import BytesIO
 import os
 import tempfile
-import streamlit as st
+from io import BytesIO
+from pydub import AudioSegment
 
-# Create a function to transcribe smaller audio files
-def transcribe_small_audio(api_key, audio_file, language="es"):
+# Create a function to transcribe audio using Whisper
+def transcribe_audio(api_key, audio_file, language="es"):
     openai.api_key = api_key
-    with BytesIO(audio_file.read()) as audio_bytes:
-        file_extension = os.path.splitext(audio_file.name)[-1]
 
-        with tempfile.NamedTemporaryFile(delete=False, suffix=file_extension) as temp_audio_file:
-            temp_audio_file.write(audio_bytes.read())
-            temp_audio_file.seek(0)  
-            transcript = openai.Audio.transcribe("whisper-1", temp_audio_file, language=language)
+    # Transcribe the audio file
+    transcript = openai.Audio.transcribe("whisper-1", audio_file, language=language)
+
     return transcript
 
-# Create a function to transcribe large audio files
-def transcribe_large_audio(api_key, audio_file, language="es"):
-    audio = pydub.AudioSegment.from_file(audio_file)
-    chunks = split_audio(audio)
-    transcripts = []
-
-    for chunk in chunks:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
-            chunk.export(temp_audio_file.name, format="wav")
-            transcript = transcribe_small_audio(api_key, temp_audio_file, language)
-            transcripts.append(transcript)
-
-    return combine_transcripts(transcripts)
-
-# Create a function to split large audio files
-def split_audio(audio, chunk_length=20*60*1000):
-    return [audio[i:i + chunk_length] for i in range(0, len(audio), chunk_length)]
-
-# Create a function to combine multiple transcripts
-def combine_transcripts(transcripts):
-    return " ".join(transcripts)
-
+# Create a function to summarize the transcript using a custom prompt
 def summarize_transcript(api_key, transcript, model, custom_prompt=None):
     openai.api_key = api_key
     prompt = f"Please summarize the following audio transcription in Spanish: {transcript}"
@@ -54,3 +29,18 @@ def summarize_transcript(api_key, transcript, model, custom_prompt=None):
 
     summary = response['choices'][0]['message']['content']
     return summary
+
+# Create a function to split an audio file using PyDub
+def split_audio_file(audio_file, chunk_size=10*60*1000):  # 10 minutes in milliseconds
+    with BytesIO(audio_file.read()) as audio_bytes:
+        audio = AudioSegment.from_file(audio_bytes)
+        chunks = [chunk for chunk in audio[::chunk_size]]
+    
+    # Create temporary files for each audio chunk
+    temp_files = []
+    for i, chunk in enumerate(chunks):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        chunk.export(temp_file.name, format='mp3')
+        temp_files.append(temp_file)
+    
+    return temp_files
